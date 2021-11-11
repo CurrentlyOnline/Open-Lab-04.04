5a6e20d3b93a8cedf17512c575a57de1b91887fd
CurrentlyOnline <sambor433@gmail.com>
2021-11-11 22:39:27 +0100

    phone number

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -6,7 +6,11 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            string first3 = "(" + numbers[0] + numbers[1] + numbers[2] + ")";
+            string last = " " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+            string finish = first3 + last;
+            return finish;
         }
+
     }
 }
